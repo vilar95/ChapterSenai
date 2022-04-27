8a0be15ee6e5055dc4655afcd8ee0d494f87e526
EDUARDO VILAR <e.vilar95@gmail.com>
2022-04-27 16:10:55 -0300

    Adicionar arquivos de projeto.

---
+++ ChapterSenai/Contexts/ChapterContext.cs
@@ -0,0 +1,31 @@
+﻿using ChapterSenai.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterSenai.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+
+        }
+        //Vamos utilizar esse método para configurar os dados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //Cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("" +
+                    "Data Source = DESKTOP-L6A6NA4\\LOCALDB#B043C77D ; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ ChapterSenai/Repositories/LivroRepository.cs
@@ -0,0 +1,24 @@
+﻿using ChapterSenai.Contexts;
+using ChapterSenai.Models;
+
+namespace ChapterSenai.Repositories
+{
+    public class LivroRepository 
+    {
+        // possui acesso aos dados
+        private readonly ChapterContext _context;
+        // somente um data context na memória da aplicação na requisição, evitar o usar o new
+        // para o repositório existir, precisa do contexto, a aplicacao cria
+        // configurar no startup
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+        // retorna a lista de livros
+        public List<Livro> Listar()
+        {
+            // SELECT Id, Titulo, QuantidadePaginas, Disponivel FROM Livros;
+            return _context.Livros.ToList();
+        }
+    }
+}
